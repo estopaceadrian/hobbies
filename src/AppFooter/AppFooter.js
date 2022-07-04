54d49f50b67ff9126e8fae0ba23dc4884539e97f
Adrian Estopace <estopaceadrian@gmail.com>
2022-07-05 01:45:21 +0800

    added footer

---
+++ src/AppFooter/AppFooter.js
@@ -0,0 +1,9 @@
+import './AppFooter.css';
+
+export default function AppFooter() {
+  return (
+    <div class="footer1">
+      <div class="col-md-12 text-center">&copy; AEstopace2022</div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppHobbies from './AppHobbies/AppHobbies';
+import AppFooter from './AppFooter/AppFooter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <AppHobbies />
+    <AppFooter />
     {/* <AppNav /> */}
   </React.StrictMode>
 );
